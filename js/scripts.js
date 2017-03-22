bca6954ed06d67da1938c8927244613ab13b0c05
Philipp Zalewski <kontakt@centrum-ngo.pl>
2017-03-22 23:02:29 +0100

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
-// plik scripts.js
-
-var withButtonClass = document.getElementsByClassName('button');
-// console.log(withButtonClass);
-
-var q = withButtonClass.length;
-console.log(withButtonClass.length);
-
-for ( var a = 0; q != a; a++) {
-
-	function textButton () {
-	console.log(withButtonClass.innerText);
+var span = $("span");
+    span.each(function(index, element) {
+	if(index % 2 == 0) {
+		$(element).css('color', 'red');
 	}
-}
+
+});
+
+
+var paragraphs = $('p');
+paragraphs.each(function(index, element) {
+
+    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+    $(element).append(button) 
+
+});
+
+$("button").click(function(){
+	alert($(this).attr("data-tmp"));
+});
